f173020ecf7c1acd074f028ec7fcb3d84baf7e45
Yaseen Latiff <Yaseenlat@gmail.com>
2023-03-11 20:02:01 +0200

    added some code to test if the templates and static files are working

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,12 +1,16 @@
 
 # A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
-from flask import render_template
+from flask import render_template, Flask, url_for, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+cors = CORS(app)
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
+if __name__ == "__main__":
+    url_for('static', filename='static\js\index.js')
+    url_for('static', filename='static\css\index.css')
